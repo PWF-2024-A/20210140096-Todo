a152ee826e1064d3895c6979a18c737c47333938
Ghani Aufa <maula.ghani.ft21@mail.umy.ac.id>
2024-03-23 16:30:18 +0700

    migration, database seeder, factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Todo;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'is_admin' => true,
+        ]);
+        User::factory()->create([
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+            'name' => 'Maula Ghani',
+            'email' => 'ghaniaufa@gmail.com',
+            'is_admin' => false,
+        ]);
+        User::factory(100)->create();
+        Todo::factory(100)->create();
     }
 }
