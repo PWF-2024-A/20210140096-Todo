eb4e66b871236d244de7387a0774395bb30f654d
Ghani Aufa <maula.ghani.ft21@mail.umy.ac.id>
2024-04-08 14:49:12 +0700

    update, delete data

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
 use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -30,11 +31,19 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
-    Route::post('/todo', [TodoController::class, 'store'])->name('todo.store');
+    Route::post('/todo/store', [TodoController::class, 'store'])->name('todo.store');
     Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
-    Route::get('/todo/edit', [TodoController::class, 'edit'])->name('todo.edit');
+    Route::get('/todo/{todo}/edit', [TodoController::class, 'edit'])->name('todo.edit');
+    Route::patch('/todo/{todo}', [TodoController::class, 'update'])->name('todo.update');
+    Route::patch('/todo/{todo}/complete', [TodoController::class, 'complete'])->name('todo.complete');
+    Route::patch('/todo/{todo}/incomplete', [TodoController::class, 'uncomplete'])->name('todo.uncomplete');
+    Route::delete('/todo/{todo}', [TodoController::class, 'destroy'])->name('todo.destroy');
+    Route::delete('/todo', [TodoController::class, 'destroyCompleted'])->name('todo.deleteallcompleted');
 
     Route::get('/user', [UserController::class, 'index'])->name('user.index');
+    Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
+    Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
+    Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeadmin'])->name('user.removeadmin');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
---
+++ tugas6.md
@@ -0,0 +1,16 @@
+## 1. Todo Completed
+![Alt text](screnshoot/tugas6/Screenshot%20(597).png)
+## 2. Todo UnCompleted
+![Alt text](screnshoot/tugas6/Screenshot%20(598).png)
+## 3. Todo Edited
+![Alt text](screnshoot/tugas6/Screenshot%20(600).png)
+## 4. Todo Deleted
+![Alt text](screnshoot/tugas6/Screenshot%20(601).png)
+## 5. Todo Delete All Completed Todo
+![Alt text](screnshoot/tugas6/Screenshot%20(602).png)
+## 6. User Make Admin
+![Alt text](screnshoot/tugas6/Screenshot%20(603).png)
+## 7. User Remove Admin
+![Alt text](screnshoot/tugas6/Screenshot%20(604).png)
+## 8. User Delete
+![Alt text](screnshoot/tugas6/Screenshot%20(605).png)
